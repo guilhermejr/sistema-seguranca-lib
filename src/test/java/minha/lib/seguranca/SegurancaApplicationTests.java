b20d656d7e428216822f5ba20c36d200c1b4ccbe
Guilherme Jr <falecom@guilhermejr.net>
2025-04-21 16:21:01 -0300

    feat: primeira versão

---
+++ src/test/java/minha/lib/seguranca/SegurancaApplicationTests.java
@@ -0,0 +1,13 @@
+package minha.lib.seguranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
